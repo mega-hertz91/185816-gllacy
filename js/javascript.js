f95247f3375ee2813fa7322af65f00b6861e6ea1
Igor Babushkin <mega-hertz@bk.ru>
2018-03-31 12:56:01 +0500

    Растяжка карты на ширину вьюпорта

--- js/javascript.js
+++ js/javascript.js
@@ -3,6 +3,8 @@
   var layer = document.querySelector(".layer");
   var modalClose = document.querySelector(".modal-close");
   var nameInput = modal.querySelector("[name=form-name]");
+  var map = document.querySelector(".main-map-adress");
+  var mapFill = document.querySelector(".map-hidden");
 
   link.addEventListener("click", function(evt) {
     evt.preventDefault();
@@ -15,4 +17,23 @@
     evt.preventDefault();
     modal.classList.remove("modal-open");
     layer.classList.remove("modal-open");
+  })
+  map.addEventListener("mouseover", function(evt){
+    evt.preventDefault();
+    map.classList.add("map-active");
+  })
+
+  map.addEventListener("mouseout", function(evt){
+    evt.preventDefault();
+    map.classList.remove("map-active");
+  })
+
+  map.addEventListener("mouseover", function(evt){
+    evt.preventDefault();
+    mapFill.classList.add("map-hidden-active");
+  })
+
+  map.addEventListener("mouseout", function(evt){
+    evt.preventDefault();
+    mapFill.classList.remove("map-hidden-active");
   })
